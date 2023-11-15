aa92a3f4e91bc701df65e1e479a8dc7d422fb10f
mahdouch9165 <100514444+mahdouch9165@users.noreply.github.com>
2023-11-15 12:42:06 -0500

    Implement zip

--- lambda_function.py
+++ lambda_function.py
@@ -23,29 +23,37 @@ def lambda_handler(event, context):
     s3.download_file('azcopy-folder', 'azcopy', '/tmp/azcopy')
     os.chmod('/tmp/azcopy', 0o755)
     
-    # AWS Bucket    
+    # AWS Bucket for cohort data
     aws_full_path = os.environ["AWS_LINK"]
+    
+    # TODO
+    # 1. Code to fetch target s3 folder
+    
+    # 2. Zip up folder and store in temporary S3 of our AWS account
     
     # Azure Blob Storage
     dest_full_path = os.environ["ADDI_TOKEN"]
 
-    azcopy_command = f'/tmp/azcopy cp "{aws_full_path}" "{dest_full_path}" --recursive=true --log-level=DEBUG'
+    # azcopy_command = f'/tmp/azcopy cp "{aws_full_path}" "{dest_full_path}" --recursive=true --log-level=DEBUG'
 
-    try:
-        result = subprocess.run(azcopy_command, check=True, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"AzCopy stdout:\n{result.stdout}")
-        print(f"AzCopy stderr:\n{result.stderr}")
-    except subprocess.CalledProcessError as e:
-        print(f"AzCopy command failed with return code {e.returncode}:")
-        print(f"AzCopy stdout:\n{e.stdout}")
-        print(f"AzCopy stderr:\n{e.stderr}")
+    # try:
+    #     result = subprocess.run(azcopy_command, check=True, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     print(f"AzCopy stdout:\n{result.stdout}")
+    #     print(f"AzCopy stderr:\n{result.stderr}")
+    # except subprocess.CalledProcessError as e:
+    #     print(f"AzCopy command failed with return code {e.returncode}:")
+    #     print(f"AzCopy stdout:\n{e.stdout}")
+    #     print(f"AzCopy stderr:\n{e.stderr}")
+        
+    # TODO
+    # 3. Delete local S3 zipped folder
         
     # Upload the logs to S3
-    log_file_path = find_azcopy_log()
-    if log_file_path:
-        upload_logs_to_s3(log_file_path)
-    else:
-        print("AzCopy log file not found")
+    # log_file_path = find_azcopy_log()
+    # if log_file_path:
+    #     upload_logs_to_s3(log_file_path)
+    # else:
+    #     print("AzCopy log file not found")
 
     return {
         'statusCode': 200,
